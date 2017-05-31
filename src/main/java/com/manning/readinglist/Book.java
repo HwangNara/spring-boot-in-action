28af0b50a04b3ed3cac385f19d0d0934f05db393
dany.dream <dany.dream@kakaocorp.com>
2017-06-01 00:18:42 +0900

    2장 완료

---
+++ src/main/java/com/manning/readinglist/Book.java
@@ -0,0 +1,28 @@
+package com.manning.readinglist;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by dany on 2017. 5. 31..
+ */
+@Setter
+@Getter
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String reader;
+    private String isbn;
+    private String title;
+    private String author;
+    private String description;
+}
